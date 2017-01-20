a7d631b7c1a68db4ce3d5c92745e5d10815cf927
Kay Illner <kay.illner@gmail.com>
2017-01-20 23:47:51 +0100

    updated light growing and shrinking behaviour

--- WaveMaze/Assets/Scripts/DarknessEffect.cs
+++ WaveMaze/Assets/Scripts/DarknessEffect.cs
@@ -2,14 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace WaveMaze {
+
 public class DarknessEffect : MonoBehaviour {
 
 	public Material material;
-
-	public void TriggerBrightness()
-	{
-		if( m_brightnessCycleTime > Mathf.PI ) m_brightnessCycleTime = 0f;
-	}
 
 	void Awake()
 	{
@@ -19,19 +16,44 @@ public class DarknessEffect : MonoBehaviour {
 	}
 
 	// Use this for initialization
-	void Start () {}
+	void Start () 
+	{
+		material.SetFloat ("_Radius", 0);
+		material.SetFloat ("_Penumbra", penumbra);	
+	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		if (m_brightnessCycleTime <= Mathf.PI) 
 		{
-			m_brightnessCycleTime += Time.deltaTime * m_brightnessCycleSpeed;
-			float scale = Mathf.Sin (m_brightnessCycleTime) * 0.5f;
+			if (m_brightnessCycleTime >= Mathf.PI / 2) {
+				m_brightnessCycleTime += Time.deltaTime * 0.2f;
+			}
+			else 
+			{
+				m_brightnessCycleTime += Time.deltaTime * m_brightnessCycleSpeed;
+			}
+			float scale = Mathf.Sin (m_brightnessCycleTime) * m_charge;
 
 			material.SetFloat ("_Radius", scale);
+		} 
+
+		if (Input.GetKey (KeyCode.Space)) 
+		{
+			ChargeFlash ();	
+			m_charge += 0.3f * Time.deltaTime;
+		}
+		else 
+		{
+			RecoverFlash ();
+			m_charge -= 0.6f * Time.deltaTime; 
+			m_charge = Mathf.Max (0.1f, m_charge);
 		}
 
-		if( Input.GetKeyDown(KeyCode.Space) ) TriggerBrightness ();
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			ReleaseFlash ();
+		}
 	}
 
 	void OnRenderImage(RenderTexture source, RenderTexture destination)
@@ -39,6 +61,35 @@ public class DarknessEffect : MonoBehaviour {
         Graphics.Blit(source, destination, material);
     }
 
-	private float m_brightnessCycleTime = 4;
-	private float m_brightnessCycleSpeed = 3f;
+	public void ChargeFlash()
+	{
+		float penumbraDecrease = 1.0f;
+		penumbra = Mathf.Lerp (penumbra, 0.1f, penumbraDecrease * Time.deltaTime);
+		material.SetFloat ("_Penumbra", penumbra);
+
+		float shakeAmount = (1.0f - penumbra / (defaultPenumbra - 0.1f)) * 0.05f;
+		material.SetFloat ( "_ShakeX", Utils.RandRange(-shakeAmount, shakeAmount) );
+	}
+
+	public void RecoverFlash()
+	{
+		float penumbraIncrease = 0.4f;
+		penumbra = Mathf.Lerp (penumbra, defaultPenumbra, penumbraIncrease * Time.deltaTime);
+		material.SetFloat ("_Penumbra", penumbra);
+	}
+
+	public void ReleaseFlash()
+	{
+		if( m_brightnessCycleTime > Mathf.PI ) m_brightnessCycleTime = 0f;
+	}
+
+	float penumbra = defaultPenumbra;
+	float m_brightnessCycleTime = 4;
+	float m_brightnessCycleSpeed = 8f;
+
+	float m_charge = 0.1f;
+
+	const float defaultPenumbra = 0.3f;
+}
+
 }
