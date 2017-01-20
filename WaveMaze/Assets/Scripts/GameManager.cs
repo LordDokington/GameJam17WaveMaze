56846a5799565d4dd2b72f2ad0d52296a93598b5
Micha <m.kropp@silverseedgames.com>
2017-01-20 21:19:47 +0100

    Added general GameFlow

---
+++ WaveMaze/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject _instructionsGO;
+    // Use this for initialization
+    void Start()
+    {
+       var instrGO = Resources.Load("Prefabs/InstructionObject") as GameObject;
+        _instructionsGO = Instantiate(instrGO);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(_instructionsGO);
+        }
+    }
+}
--- WaveMaze/Assets/Scripts/MenuBehaviour.cs
+++ WaveMaze/Assets/Scripts/MenuBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuBehaviour : MonoBehaviour
 {
@@ -17,7 +18,7 @@ public class MenuBehaviour : MonoBehaviour
 
     public void OnStartGameClicked()
     {
-
+        SceneManager.LoadScene("GameScene");
     }
 
     public void OnExitGameClicked()
