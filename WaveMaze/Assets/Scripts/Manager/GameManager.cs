ac854aa31f70d41c7e5862f1c530251c0f9fa4c7
Micha <m.kropp@silverseedgames.com>
2017-01-20 22:01:14 +0100

    added more folders and some scripts

--- WaveMaze/Assets/Scripts/Manager/GameManager.cs
+++ WaveMaze/Assets/Scripts/Manager/GameManager.cs
@@ -2,22 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : SingletonBehaviour<GameManager>
 {
+    private bool _isInstructionMode;
     private GameObject _instructionsGO;
+
+    public bool GetInstructionMode
+    {
+        get { return _isInstructionMode; }
+    }
+
     // Use this for initialization
     void Start()
     {
        var instrGO = Resources.Load("Prefabs/InstructionObject") as GameObject;
         _instructionsGO = Instantiate(instrGO);
+        _isInstructionMode = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(_isInstructionMode && Input.GetKeyDown(KeyCode.Space))
         {
             Destroy(_instructionsGO);
+            _isInstructionMode = false;
         }
     }
 }
