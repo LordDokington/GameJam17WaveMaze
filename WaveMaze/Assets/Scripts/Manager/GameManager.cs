d6311534cb52546efe74e072193aff67a4ee2b2f
Micha <m.kropp@silverseedgames.com>
2017-01-21 20:08:49 +0100

    added 2nd enemie type with smoke

--- WaveMaze/Assets/Scripts/Manager/GameManager.cs
+++ WaveMaze/Assets/Scripts/Manager/GameManager.cs
@@ -17,6 +17,9 @@ public class GameManager : SingletonBehaviour<GameManager>
 
     public GameState CurrentState;
     public bool FinishPreloading;
+
+    private GameObject _player1;
+    private GameObject _player2;
     private GameData m_GameData;
 
     public GameData GetGameData
@@ -25,6 +28,16 @@ public class GameManager : SingletonBehaviour<GameManager>
         {
             return m_GameData;
         }
+    }
+
+    public GameObject Player1
+    {
+        get { return _player1; }
+    }
+
+    public GameObject Player2
+    {
+        get { return _player2; }
     }
 
     public override void AwakeSingleton()
@@ -49,5 +62,25 @@ public class GameManager : SingletonBehaviour<GameManager>
     void Update()
     {
 
+    }
+
+
+    public void FindPlayer()
+    {
+        var players = GameObject.FindGameObjectsWithTag("Player");
+        _player1 = players[0];
+        if(players.Length > 1)
+            _player2 = players[1];
+    }
+
+    public void StartOutro()
+    {
+
+    }
+
+    public void ShouldCameraDarknessBeOn(bool state)
+    {
+        var mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        mainCamera.GetComponent<WaveMaze.DarknessEffect>().enabled = state;
     }
 }
