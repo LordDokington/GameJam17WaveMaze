197d4259fcf3d5c60408d43df2972d1a70d61716
Kay Illner <kay.illner@gmail.com>
2017-01-21 15:30:17 +0100

    updated camera, darkness effect, scene

--- WaveMaze/Assets/Scripts/LevelManager.cs
+++ WaveMaze/Assets/Scripts/LevelManager.cs
@@ -14,12 +14,12 @@ namespace WaveMaze{
 		// Use this for initialization
 		void Start() {
             
-            m_CurrentLevelGo = new GameObject("m_CurrentLevelGo");
+            m_CurrentLevelGo = new GameObject("CurrentLevelGo");
             m_SpriteRenderer = m_CurrentLevelGo.AddComponent<SpriteRenderer>();
             m_SpriteRenderer.sortingLayerName = "LevelLayer";
             addLevel(1);
             GameManager.Instance.FinishPreloading = true;
-            StartLeve(1);
+            StartLevel(1);
         }
 		
 		// Update is called once per frame
@@ -27,25 +27,28 @@ namespace WaveMaze{
 			
 		}
 
-		void addLevel(int LevelCount){
-			if (m_LevelList == null) {
+		void addLevel(int LevelCount)
+		{
+			if (m_LevelList == null) 
+			{
 				m_LevelList = new List<LevelHandler>();
 			}
-			m_LevelList.Add( new LevelHandler(LevelCount));
+			m_LevelList.Add( new LevelHandler(LevelCount) );
 		}
 
 		public void LoadLevel(int TheLevel){
 			addLevel(TheLevel);
 		}
 
-        public void StartLeve(int TheLevel) {
+        public void StartLevel(int TheLevel) 
+		{
             StartTransition();
             m_CurrentLevel = TheLevel;
             m_CurrentLevelGoundTexture = m_LevelList.Find(x => x.m_LevelNumber == TheLevel).LevelGround;
             
             Debug.Log("Width: " + m_CurrentLevelGoundTexture.rect.height.ToString() + "Width: " + m_CurrentLevelGoundTexture.rect.width.ToString());
-            m_SpriteRenderer.sprite = m_CurrentLevelGoundTexture;
-            m_SpriteRenderer.material = new Material( Shader.Find("Diffuse") );
+
+			m_SpriteRenderer.sprite = m_CurrentLevelGoundTexture;
         }
 
         void StartTransition() {
