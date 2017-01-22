d65f23268f8b68b804bad46ed4b6ba4f28b00c07
Kay Illner <kay.illner@gmail.com>
2017-01-22 16:26:08 +0100

    enemy waves first version

---
+++ WaveMaze/Assets/Scripts/EnemyReaction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyReaction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GameObject cam = GameObject.FindGameObjectWithTag ("MainCamera");
+		cam.GetComponent<EnemyLightReaction>().EnemyPosition1 = transform.position;
+		
+	}
+}
