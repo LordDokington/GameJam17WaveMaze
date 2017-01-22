8f8908247688c143eafd3ad83dd7197151b37675
Micha <m.kropp@silverseedgames.com>
2017-01-22 11:40:47 +0100

    added DescriptionBehaviour.cs

---
+++ WaveMaze/Assets/Scripts/ObjectBehaviour/DescriptionBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WaveMaze;
+
+public class DescriptionBehaviour : MonoBehaviour
+{
+    public Camera mainCam;
+    public Transform LightSourceOne;
+    public Transform LightSourceTwo;
+
+    public void SetLightStatus(bool ShouldBeActive)
+    {
+        DarknessEffect darkScript = mainCam.GetComponent<DarknessEffect>();
+        if (ShouldBeActive)
+        {
+            /*
+            darkScript.EnemyGlowPosition = LightSourceOne.position;
+            darkScript.EnemyGlowPosition = LightSourceTwo.position;
+            Set Radius
+            */
+
+        }
+    }
+}
