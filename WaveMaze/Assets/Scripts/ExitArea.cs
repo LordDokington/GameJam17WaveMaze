041b32addb4804d0abd3734899729626fe2084e2
Seraphim137 <andrew.burke86@googlemail.com>
2017-01-22 07:18:23 +0100

    AddFleetPath

--- WaveMaze/Assets/Scripts/ExitArea.cs
+++ WaveMaze/Assets/Scripts/ExitArea.cs
@@ -1,18 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class ExitArea : MonoBehaviour {
 
     public GameObject TriggeredPlayer;
     public GameObject CollectedPlayer;
 
-    public float Distance = 0.8f;
+    public float CollectDistance = 0.8f;
+    public float FollowDistance = 1f;
     public float MoveSpeed = 8f;
+    public float FleetSpeed = 8.5f;
 
     bool CollectedMoveStart = false;
     bool CollectedMoveFinisched = false;
 
+    bool FleetMoveStart = false;
+    bool FleetMoveFinisched = false;
+
+    int Etappe = 0;
+
+    int nextFleetPointIndex = 0;
+    public int FleetPointCount = 0;
+    List<GameObject> FleetPointList;
+    public GameObject RegFleetpoint{
+        set {
+            if (FleetPointList == null) {
+                FleetPointList = new List<GameObject>();
+            }
+            FleetPointCount++;
+            FleetPointList.Add(value);
+            FleetPointList.Sort(delegate (GameObject a, GameObject b) {
+                return a.transform.position.x.CompareTo(b.transform.position.x);
+            });
+            
+        }
+    }
+
+    public GameObject FinischPoint;
     // Use this for initialization
     void Start () {
 		
@@ -20,11 +46,18 @@ public class ExitArea : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        collectPlayer();
+        FleetPlayer();
+    }
+
+    void collectPlayer() {
         if (CollectedMoveStart && !CollectedMoveFinisched)
         {
+            TriggeredPlayer.GetComponent<PlayerController>().enabled = false;
+            CollectedPlayer.GetComponent<PlayerController>().enabled = false;
             float aDistance = Vector3.Distance(TriggeredPlayer.transform.position, CollectedPlayer.transform.position);
-                Debug.Log(aDistance.ToString());
-            if ( aDistance < Distance)
+            //Debug.Log(aDistance.ToString());
+            if (aDistance < CollectDistance)
             {
                 CollectedMoveStart = false;
                 CollectedMoveFinisched = true;
@@ -33,14 +66,61 @@ public class ExitArea : MonoBehaviour {
             {
                 CollectedPlayer.transform.position += (TriggeredPlayer.transform.position - CollectedPlayer.transform.position).normalized * MoveSpeed * Time.deltaTime;
             }
+
+           if (aDistance < FollowDistance)
+           {
+                FleetMoveStart = true;
+           }
         }
 
         if (CollectedMoveFinisched)
         {
-            StartTransition();
+            CollectedMoveFinisched = true;
         }
-
     }
+
+    void FleetPlayer()
+    {
+        //Debug.Log("PlayerController enable " + TriggeredPlayer.GetComponent<PlayerController>().enabled);
+       
+        if (FleetMoveStart && !FleetMoveFinisched)
+        {
+            Vector3 atarget;
+            if (nextFleetPointIndex < FleetPointList.Count)
+            {
+                atarget = FleetPointList[nextFleetPointIndex].transform.position;
+            }
+            else
+            {
+                atarget = FinischPoint.transform.position;
+            }
+
+            float aDistance = Vector3.Distance(TriggeredPlayer.transform.position, atarget);
+            Debug.Log(aDistance.ToString() + " " + nextFleetPointIndex.ToString());
+            if (aDistance < CollectDistance)
+            {
+                nextFleetPointIndex++;
+                if (atarget == FinischPoint.transform.position)
+                {
+                    FleetMoveStart = false;
+                    FleetMoveFinisched = true;
+                }
+            }
+
+            if (FleetMoveStart)
+            {
+                TriggeredPlayer.transform.position += (atarget - TriggeredPlayer.transform.position).normalized * FleetSpeed * Time.deltaTime;
+            }
+
+            if (FleetMoveFinisched)
+            {
+                FleetMoveFinisched = false;
+                TriggeredPlayer.GetComponent<PlayerController>().enabled = false;
+                CollectedPlayer.GetComponent<PlayerController>().enabled = false;
+            }
+        }
+    }
+
     //void Test(Collision collision)
     private void OnTriggerEnter2D(Collider2D collision)
     {
---
+++ WaveMaze/Assets/Scripts/RegisterFleetPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegisterFleetPoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponentInParent<ExitArea>().RegFleetpoint = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
