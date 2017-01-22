3d5d6fc4c25b6c6a0762fac5b18dd91a1e498ae2
Micha <m.kropp@silverseedgames.com>
2017-01-22 12:14:48 +0100

    only disable testpoints in release

--- WaveMaze/Assets/Scripts/ObjectBehaviour/DeactivateAllSpritesInChildren.cs
+++ WaveMaze/Assets/Scripts/ObjectBehaviour/DeactivateAllSpritesInChildren.cs
@@ -7,11 +7,15 @@ public class DeactivateAllSpritesInChildren : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+#if UNITY_EDITOR
+
+#else
         var spriteRs = transform.GetComponentsInChildren<SpriteRenderer>();
         foreach (var renderer in spriteRs)
         {
             renderer.enabled = false;
         }
+#endif
     }
 
 }
