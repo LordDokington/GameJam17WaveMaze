3406cf0c5693d7d38bd0c5810a5235124f5fa6ad
Seraphim137 <andrew.burke86@googlemail.com>
2017-01-22 01:54:36 +0100

    moveTriggerscripts

---
+++ WaveMaze/Assets/Scripts/EntryArea.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntryArea : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollision2D(Collision2D collision)
+    {
+        string aName = collision.gameObject.name;
+        if (aName.Equals("Player1") || aName.Equals("Player2"))
+        {
+            Debug.Log(this.name + "EntryArea triggered");
+            this.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+
+}
---
+++ WaveMaze/Assets/Scripts/ExitArea.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitArea : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollision2D(Collision2D collision)
+    {
+        string aName = collision.gameObject.name;
+        if (  aName.Equals("Player1") || aName.Equals("Player2") )
+        {
+            Debug.Log(this.name + "EntryArea triggered");
+            this.GetComponent<BoxCollider2D>().enabled = false;
+            CollectPlayer();
+            StratTransition();
+        }
+    }
+
+    void StratTransition()
+    {
+
+    }
+
+    void CollectPlayer()
+    {
+
+    }
+}
